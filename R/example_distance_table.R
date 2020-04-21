392ed15997d038b61f06e1f3d41f455431097b41
SCSchmidt <s.c.schmidt@uni-koeln.de>
2020-04-21 11:21:03 +0200

    more data documentation files.

--- R/example_distance_table.R
+++ R/example_distance_table.R
@@ -1,22 +1,24 @@
-#' Atlas of Hillforts of Britain and Ireland data 
+#' Atlas of Hillforts of Britain and Ireland data: distance table
 #' 
-#' The example point coordinate data is taken from the:
+#' The example distance table is the square root of the point coordinate data taken from the:
 #' Atlas of Hillforts of Britain and Ireland 
 #' Lock, G. and Ralston, I. 2017.  Atlas of Hillforts of Britain and Ireland. [ONLINE] Available at: https://hillforts.arch.ox.ac.uk
 #' This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License
 #' The point coordinate data was extracted from the database for hillforts in Britain (England, Scotland, Wales and the Isle of Man) which have status of interpretation as "confirmed". This was extracted on: 25/07/2017
-
+#' 
 #' For example of usage, please see the vignette.
 
+
+#'
 #' @docType data
 #'
 #' @usage data(data)
 #'
-#' @format Object of class \code{"Dataframe"} called "data" 
+#' @format Object of class \code{"Dataframe"} called "distance_table" 
 #'
 #' @keywords datasets
 #'
 #' @references Atlas of Hillforts of Britain and Ireland, Lock, G. and Ralston, I. 2017.  Atlas of Hillforts of Britain and Ireland. [ONLINE] Available at: https://hillforts.arch.ox.ac.uk
 #'
 #'
-"data"
+"distance_table"
---
+++ R/example_shape.R
@@ -0,0 +1,21 @@
+#' Shapefile: Outline of Great Britain
+#'
+#' 
+#' Ordnance Survey OS Open Data, uner a UK Open Government Licence: https://www.ordnancesurvey.co.uk/opendatadownload/products.html#OVERGB
+#' This file Contains OS data © Crown Copyright (2020)
+#' For example of usage, please see the vignette.
+
+
+#'
+#' @docType data
+#'
+#' @usage data(data)
+#'
+#' @format Object of class \code{"Large SpatialPolygonsDataFrame"} called "shape
+#'
+#' @keywords datasets
+#'
+#' @references Ordnance Survey OS Open Data, uner a UK Open Government Licence: https://www.ordnancesurvey.co.uk/opendatadownload/products.html#OVERGB
+#'
+#'
+"shape"
